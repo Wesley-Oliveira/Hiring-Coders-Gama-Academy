0cfab46acb7f615687fc933750ba85ceae70502a
Wesley-Oliveira <wesley0leby@gmail.com>
2020-06-01 19:14:44 -0300

    Installing and configuring react-router-dom

--- React/gamagit/src/App.js
+++ React/gamagit/src/App.js
@@ -1,23 +1,10 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React from 'react';
 
 function App() {
-  const [ usuario, setUsuario ] = useState('');
-  
-  function handlePesquisa() {
-      axios.get(`https://api.github.com/users/${usuario}/repos`).then(response => console.log(response.data));
-  }
-
   return ( 
     <>
-      <input className="usuarioInput" placeholder="Usuário" value={usuario} onChange={e => setUsuario(e.target.value)}/>
-      <button type="button" onClick={handlePesquisa}>Pesquisar</button>
     </>
   );
 }
 
 export default App;
-
-
-//JSX
-// State vai retornar [usuario, setUsuario]
