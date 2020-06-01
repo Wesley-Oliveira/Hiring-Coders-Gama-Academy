ad63da4013e4bc309d5da9996c4937b426f81531
Wesley-Oliveira <wesley0leby@gmail.com>
2020-06-01 11:11:43 -0300

    React basics

---
+++ React/gamagit/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App() {
+  return (
+    <>
+      <input name="usuario" id="usuario" className="usuarioInput" placeholder="Usuário" />
+      <button type="button">Pesquisar</button>
+    </>
+  );
+}
+
+export default App;
+
+
+//JSX
+// State vai retornar [usuario, setUsuario]
