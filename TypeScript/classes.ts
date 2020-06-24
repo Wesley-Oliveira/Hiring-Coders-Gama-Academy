aa10fea2f05635fcb051b543b7d6df9abb87084f
Wesley-Oliveira <wesley0leby@gmail.com>
2020-06-24 17:10:13 -0300

    Using access modifiers, inheritance

--- TypeScript/classes.ts
+++ TypeScript/classes.ts
@@ -21,3 +21,52 @@ console.log(data.dia);
 
 const data2 = new Data(1, 1); //testando com valor default definido no construtor
 console.log(data2);
+
+
+class Carro {
+    private velocidadeAtual: number = 0;
+
+    constructor(
+        public marca: string,
+        public modelo: string,
+        private velocidadeMaxima: number = 220
+    ) { }
+
+    private alterarvelocidade(delta: number) {
+        const novaVelocidade = this.velocidadeAtual + delta;
+
+        if(novaVelocidade >= 0 && novaVelocidade <= this.velocidadeMaxima) {
+            this.velocidadeAtual = novaVelocidade;
+        } else {
+            this.velocidadeAtual = delta > 0 ? this.velocidadeMaxima : 0;
+        }
+    }
+
+    acelerar() {
+        this.alterarvelocidade(5);
+    }
+
+    frear() {
+        this.alterarvelocidade(-5);
+    }
+}
+
+const carro = new Carro('Chevrolet', 'Prisma', 250);
+carro.acelerar();
+
+// Herança
+class Camaro extends Carro {
+    private turbo = false
+    constructor() {
+        super('Chevrolet', 'Camaro', 500);
+    }
+
+    ligarTurbo() {
+        this.turbo = true;
+    }
+}
+
+const camaro = new Camaro();
+camaro.acelerar();
+camaro.frear();
+camaro.ligarTurbo();
